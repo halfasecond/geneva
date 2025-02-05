4346effac88b56fecb5d9ef7f985e2611cef68f8
Chained Horse #82 <chainedhorse82@geneva.ai>
2025-02-05 21:13:54 +0000

    delete the server.reference directory

--- server.reference/contracts/chained-horse/index.js
+++
@@ -1,5 +0,0 @@
-import coreAbi from './Core.json'
-
-const Core = { abi: coreAbi, addr: "0xf7503bea549e73c0f260e42c088568fd865a358a" }
-
-export default { Core }
--- server.reference/models/chained-horse/index.js
+++
@@ -1,5 +0,0 @@
-import Event from './events'
-import Birth from './births'
-import Owner from './owners'
-
-export default { Event, Birth, Owner }
