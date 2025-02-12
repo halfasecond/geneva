c02601b056c24730f08055ca51d9cf9f92f7f6a6
Chained Horse #82 <chainedhorse82@geneva.ai>
2025-02-12 12:08:46 +0000

    refactor: remove unnecessary Router usage
    - Remove BrowserRouter since no routing is used
    - Remove ScrollToTop component and useLocation hook
    - Simplify component structure
    - Fix React Router warnings in console

--- src/components/App/AppView.tsx
+++ src/components/App/AppView.tsx
@@ -1,6 +1,5 @@
 import * as Styled from '../../style'
-import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 import Metamask from '../Metamask'
 import { AuthProps } from '../../types/auth'
 import { Paddock } from 'components/Paddock'
@@ -12,25 +11,17 @@ const AppView: React.FC<AuthProps> = ({ handleSignIn, handleSignOut, loggedIn: w
     const handleStart = () => {
         setShowIntro(false);
     };
+
     return (
-        <Router basename={BASE_URL}>
-            <ScrollToTop />
+        <>
             <Metamask {...{ handleSignIn, handleSignOut, BASE_URL }} loggedIn={walletAddress} />
             {showIntro && <IntroModal onStart={handleStart} />}
             <Styled.Main>
                 <h1>The Paddock</h1>
                 <Paddock horseId="21" modalOpen={showIntro} />
             </Styled.Main>
-        </Router>
+        </>
     )
-}
-
-const ScrollToTop: React.FC = () => {
-    const { pathname } = useLocation()
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' })
-    }, [pathname])
-    return null
 }
 
 export default AppView
