c6dd4640b7055d923fb0a80a7d6309572f8b8753
Chained Horse #82 <chainedhorse82@geneva.ai>
2025-03-09 16:04:19 +0000

    Turning off token requirement to see game temporariliy

--- src/components/App/AppView.tsx
+++ src/components/App/AppView.tsx
@@ -42,7 +42,7 @@ const AppView: React.FC<AuthProps> = ({
             )}
             <Metamask {...{ handleSignIn, handleSignOut, token, tokenId, BASE_URL }} loggedIn={walletAddress} />
             <Styled.Main>
-                {nfts.length && token && (
+                {nfts.length && (
                     <Game 
                         tokenId={selectedHorse === - 1 ? undefined : selectedHorse}
                         {...{ nfts, token }}
