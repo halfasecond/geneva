3e2d9529dca86fc13bab7da44e73f1ec633d429c
hasjack <jack@half-a-second.com>
2025-06-29 20:05:50 +0100

    patch related to auto scroll when on more letterbox shaped browsers

--- src/components/Purr/style.ts
+++ src/components/Purr/style.ts
@@ -140,7 +140,7 @@ export const Main = styled.main`
             flex-direction: column;
             flex-grow: 1;
             align-items: flex-start;
-            justify-content: center;
+            justify-content: flex-start;
             width: 90%;
             background-color:rgba(255,255,255,0.06);
             box-sizing: border-box;
